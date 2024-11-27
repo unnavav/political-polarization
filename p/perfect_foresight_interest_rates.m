ffb63b87989e57ee74e7870ae78e55c5e2cc946f
unnavav <unnava.v@gmail.com>
2024-11-27 15:54:42 -0500

    Trying transition dynamics

--- p/perfect_foresight_interest_rates.m
+++ p/perfect_foresight_interest_rates.m
@@ -22,6 +22,9 @@ GP = G;
 adistrP = adistr;
 rP = r;
 
+load terms_struct_t0.1500_eta1.0000.mat
+termsP = terms;
+
 load results_t0.0500_eta1.0500.mat
 
 VL = V;
@@ -29,10 +32,37 @@ GL = G;
 adistrL = adistr;
 rL = r;
 
+load terms_struct_t0.0500_eta1.1000.mat
+termsL = terms;
+
+load results_transition_t0.0500_eta1.1000.mat
+VLP = V;
+GLP = G;
+adistrLP = adistr;
+rLP = r;
+
+load results_transition_t0.1500_eta1.0000.mat
+VPL = V;
+GPL = G;
+adistrPL = adistr;
+rPL = r;
+
 cd ../../p
 
 %% now to do perfect foresight transitions
 
 % give a series of interest rates that switch partway through
 
-R = [repelem(rP, 50), repelem(rL, 400)];
+%lagg will change; first period and then the growth rate changes, 
+% which means new prices for everything
+
+lP=1.2145; lL = 1.3359; % pulling these manually bc i forgot to save them
+
+lt = [repelem(lP, 25), repelem(lL, 200)];
+
+rstart = rP;
+rend = rL;
+
+kst = ((rstart+delta)/alpha)^(1/(alpha-1))*lP;
+k0 = ((rend+delta)/alpha)^(1/(alpha-1))*lL;
+
